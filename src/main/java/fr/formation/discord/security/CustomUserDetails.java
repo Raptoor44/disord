a006b12bb6f23659f71bf5019cbf01a346f8600f
Joachim Ecole <joachim.guitton@gmail.com>
2023-09-26 14:56:49 +0200

    Implémentation officielle de la sécurité.

---
+++ src/main/java/fr/formation/discord/security/CustomUserDetails.java
@@ -0,0 +1,55 @@
+package fr.formation.discord.security;
+
+import fr.formation.discord.models.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUserDetails implements UserDetails {
+
+    private User user; // Assurez-vous d'avoir une classe User
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+
+    // Implémentez les méthodes de UserDetails en utilisant les données de votre modèle User.
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"));
+    }
+
+    // Implémentez les autres méthodes UserDetails si nécessaire.
+}
